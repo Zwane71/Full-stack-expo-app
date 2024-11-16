fbce404c0113bf9841ae39fa2a19278ea8807d74
MakZwane71 <makhohlisathemba@gmail.com>
2024-11-16 11:55:14 +0200

    building custom components

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,35 +1,14 @@
-import { Colors } from "@/src/constants/Colors";
-import { Image, StyleSheet, Platform, View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import products from "@/assets/data/products";
+import ProductListItem from "@/src/components/ProductListItem";
 
-const product = products[0];
-export default function HomeScreen() {
+export default function MenuScreen() {
 	return (
-		<SafeAreaView style={styles.container}>
-			<Image source={{ uri: product.image }} style={styles.image} />
-			<Text style={styles.title}>{product.name}</Text>
-			<Text style={styles.price}>${product.price}</Text>
+		<SafeAreaView>
+			<ProductListItem product={products[5]} />
+			<ProductListItem product={products[1]} />
 		</SafeAreaView>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		padding: 10,
-		borderRadius: 20,
-	},
-	title: {
-		fontSize: 18,
-		fontWeight: "600",
-		marginVertical: 10,
-	},
-	price: {
-		color: Colors.light.tint,
-		fontWeight: "bold",
-	},
-	image: {
-		width: "100%",
-		aspectRatio: 1,
-	},
-});
+c;
